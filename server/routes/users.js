14f10f79674f9ee242160246a64dc3b7648c2aae
shodon <about@shodon.ru>
2024-03-02 19:40:14 +0700

    add strict equality

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,7 +7,7 @@ const regRouter = express.Router()
 regRouter.post('signup', async (req, res, next) => {
   const { avatar, name, des, email, password } = req.body
 
-  if (i in [name, email, password] == null){
+  if (i in [name, email, password] === null){
     return res.status(400).json({ err: "All of these field must be filled"})
   }
 })
